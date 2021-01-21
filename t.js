cfd0814d1d922f37134b5b81fd571ac86e9d6751
LucisBaoshg <baoshg@126.com>
2021-01-21 14:25:54 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,15 +1,7 @@
-let _win_svc = context.getSystemService(context.WINDOW_SERVICE);
-let _win_svc_disp = _win_svc.getDefaultDisplay();
-let _H = _win_svc_disp.getHeight()
-let _W = _win_svc_disp.getWidth()
+auto.waitFor();
 
-app.launch("com.zhiliaoapp.musically")
+sleep(1000);
 
-sleep(2000)
-i=7
-while(i!==0){
-    swipe(_W/2, _H/3*2, _W/2, _H/8, 1000)
-    i=i-1
-    var c = random(10000,15000)
-    sleep(c)
-    }
+if(!device.isScreenOn()){
+    device.wakeUp()
+}
